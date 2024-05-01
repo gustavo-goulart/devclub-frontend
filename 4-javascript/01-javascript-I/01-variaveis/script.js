f0bf92aa87cb78bec242103e8f6c487c79ac53b8
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-01 13:22:44 -0300

    inclusoes de variáveis

--- 4-javascript/01-javascript-I/01-variaveis/script.js
+++ 4-javascript/01-javascript-I/01-variaveis/script.js
@@ -1,1 +1,23 @@
 console.log("Olá Mundo!");
+
+var age = 27;
+
+console.log(age);
+
+let name = "Rodolfo";
+
+if (true) {
+  let name = "Stephani";
+  console.log(name); // retornará stephani
+}
+
+console.log(name); // retornará rodolfo
+// Stephani
+//Rodolfo
+
+const name1 = "Stephani";
+
+name1 = "Rodolfo";
+
+console.log(name1);
+//TypeError: Assignment to constant variable.
