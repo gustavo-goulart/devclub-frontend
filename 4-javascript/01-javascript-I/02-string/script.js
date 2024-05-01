29fba6234dac8c463bcc0e2383e3cc3473fe6579
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-01 13:27:44 -0300

    strings

---
+++ 4-javascript/01-javascript-I/02-string/script.js
@@ -0,0 +1,11 @@
+let mensagem = "Olá, mundo";
+
+console.log(mensagem);
+//Olá, mundo!
+
+let nome = "Stephani";
+let idade = 27;
+let frase = `Meu nome é ${nome} e tenho ${idade} anos`;
+
+console.log(frase);
+// Meu nome é Stephani e tenho 27 anos
