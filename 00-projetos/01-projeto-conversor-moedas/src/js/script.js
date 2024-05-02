4c9ec6852e7e4a5731efea16279804e4f8c90529
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-02 16:42:46 -0300

    estrutura js

--- 00-projetos/01-projeto-conversor-moedas/src/js/script.js
+++ 00-projetos/01-projeto-conversor-moedas/src/js/script.js
@@ -1,1 +1,13 @@
-console.log("TESTE");
+const convertButton = document.querySelector(".convert-button");
+
+function convertValues() {
+  const inputCurrencyValue = document.querySelector(".input-currency").value;
+
+  const dolarToday = 5.2;
+
+  const convertedValue = inputCurrencyValue / dolarToday;
+
+  console.log(convertedValue);
+}
+
+convertButton.addEventListener("click", convertValues);
