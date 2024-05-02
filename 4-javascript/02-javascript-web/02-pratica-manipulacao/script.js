31652ddecf7478872f3d49cab3836dcd5ac197c7
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-02 14:22:45 -0300

    Elementos

--- 4-javascript/02-javascript-web/02-pratica-manipulacao/script.js
+++ 4-javascript/02-javascript-web/02-pratica-manipulacao/script.js
@@ -1,8 +1,9 @@
-// const button = document.querySelector(".main-button");
-const button = document.querySelector("h1");
+const input = document.querySelector("#main-input");
 
-// button.style.color = "#000";
+function cliqueiNoBotao() {
+  console.log(input.value);
+}
 
-//propriedades q tenham hífen descreve com camel case background-color -> backgroundColor
-// button.style.backgroundColor = "#852394";
-button.style.fontSize = "58px";
+function digiteiNoInput() {
+  console.log("Digitei no Input");
+}
