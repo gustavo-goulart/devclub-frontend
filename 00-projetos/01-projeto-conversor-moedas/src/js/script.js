123ff5d22a4400d606b3a442dfcd7423d7f9f807
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-02 17:02:48 -0300

    continuação js

--- 00-projetos/01-projeto-conversor-moedas/src/js/script.js
+++ 00-projetos/01-projeto-conversor-moedas/src/js/script.js
@@ -1,4 +1,8 @@
 const convertButton = document.querySelector(".convert-button");
+const currencyValueToConvert = document.querySelector(
+  ".currency-value-to-convert"
+); // valor em R$
+const currencyValueConverted = document.querySelector(".currency-value"); //outras moedas
 
 function convertValues() {
   const inputCurrencyValue = document.querySelector(".input-currency").value;
@@ -7,7 +11,14 @@ function convertValues() {
 
   const convertedValue = inputCurrencyValue / dolarToday;
 
-  console.log(convertedValue);
+  currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(inputCurrencyValue);
+  currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(convertedValue);
 }
 
 convertButton.addEventListener("click", convertValues);
