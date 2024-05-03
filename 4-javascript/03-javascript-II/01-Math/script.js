b83edb620182ed772b3214b9ebe4404625defcc7
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-03 14:04:03 -0300

    Math

--- 4-javascript/03-javascript-II/01-Math/script.js
+++ 4-javascript/03-javascript-II/01-Math/script.js
@@ -1,1 +1,15 @@
-console.log("teste");
+// const result = Math.pow(2, 2);
+
+// const result = Math.sqrt(25, 2);
+
+// const result = Math.PI;
+
+//ceil arredonda pra cima
+// const result = Math.ceil(3.1415);
+
+//floor arredonda pra baixo
+// const result = Math.floor(3.1415);
+
+//ramdom - número aleatório entre 0 e 1
+const result = Math.random();
+console.log(result);
