e94502cbbbd099ee028c25851ee1394201afe5dd
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-04 14:41:21 -0300

    funcao return

---
+++ 4-javascript/04-javascript-III/06-function-return/script.js
@@ -0,0 +1,15 @@
+//funções VOID - Não retornam argumentos
+// function sum(value, value2) {
+//   console.log(value + value2);
+// }
+
+// const result = sum(10, 20);
+// console.log(result + " o resultado retornou UNDFINED");
+
+//Função Return -> há um retorno quando chamada
+function sub(value, value2) {
+  return value - value2;
+}
+
+const result = sub(30, 20);
+console.log(result + " o resultado retornou a conta feita");
