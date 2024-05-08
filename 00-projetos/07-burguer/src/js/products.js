80b54b5741f3135bdeb680876c12e02a13ef0abe
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-08 14:03:29 -0300

    alteração caminho imagens

---
+++ 00-projetos/07-burguer/src/js/products.js
@@ -0,0 +1,38 @@
+const menuOptions = [
+  {
+    name: "X-Salada",
+    price: 30,
+    vegan: false,
+    src: "../assets/images/xsalada.jpeg",
+  },
+  {
+    name: "X-Bacon",
+    price: 34,
+    vegan: false,
+    src: "../assets/images/xbacon.png",
+  },
+  {
+    name: "X-Bacon Egg",
+    price: 39,
+    vegan: false,
+    src: "../assets/images/bacon-egg.png",
+  },
+  {
+    name: "Monstruoso",
+    price: 50,
+    vegan: false,
+    src: "../assets/images/monstruoso.png",
+  },
+  {
+    name: "Big Vegano",
+    price: 55,
+    vegan: true,
+    src: "../assets/images/xvegan.png",
+  },
+  {
+    name: "X-Vegan",
+    price: 45,
+    vegan: true,
+    src: "../assets/images/monstruoso-vegan.png",
+  },
+];
