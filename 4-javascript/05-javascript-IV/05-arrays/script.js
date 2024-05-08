cc2b3f1f0e552b8aa4e43e38aedb52b5b3e74c75
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-08 13:39:36 -0300

    tipos de arrays

---
+++ 4-javascript/05-javascript-IV/05-arrays/script.js
@@ -0,0 +1,15 @@
+// .push() -> adiciona itens no array na ÚLTIMA POSIÇÃO do array
+const myArray = [1, 2, 3];
+
+//informar oq quero adicionar
+myArray.push("Gustavo");
+
+console.log(myArray);
+
+//.length() -> Mostra o tamanho do array. A quantidade de itens dentro do array.
+const myArray1 = [1, 2, 3];
+console.log(myArray1.length);
+
+//.sort: Organiza o array
+let array = ["mamão", "abacate", "laranja"];
+array.sort();
