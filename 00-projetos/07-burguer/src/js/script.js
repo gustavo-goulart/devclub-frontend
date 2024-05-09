a2f8445ad490c3e3e9ad52be024a1d588a5b4d84
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-09 12:20:53 -0300

    filter

--- 00-projetos/07-burguer/src/js/script.js
+++ 00-projetos/07-burguer/src/js/script.js
@@ -1,6 +1,18 @@
 const list = document.querySelector("ul");
 const buttonShowAll = document.querySelector(".show-all");
 const buttonMapAll = document.querySelector(".map-all");
+const buttonSumAll = document.querySelector(".sum-all");
+const buttonFilterAll = document.querySelector(".filter-all");
+
+//formatar estilo de moeda
+function formatCurrency(value) {
+  const newValue = value.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  return newValue;
+}
 
 function showAll(productsArray) {
   let myLi = ""; //resetar
@@ -10,7 +22,7 @@ function showAll(productsArray) {
     <li>
       <img src=${product.src} />
       <p>${product.name}</p>
-      <p class="item-price">R$ ${product.price}</p>
+      <p class="item-price">${formatCurrency(product.price)}</p>
     </li>
     
     `;
@@ -29,5 +41,26 @@ function mapAllItems() {
   showAll(newPrices);
 }
 
+function sumAllItems() {
+  const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0);
+
+  list.innerHTML = `
+  <li>
+    <p>A soma dos valores dos itens é de ${formatCurrency(totalValue)}</p>
+  </li>
+  
+  `;
+}
+
+function filterAllItems() {
+  const filterJustVegan = menuOptions.filter(
+    (product) => product.vegan === true
+  );
+
+  showAll(filterJustVegan);
+}
+
 buttonShowAll.addEventListener("click", () => showAll(menuOptions));
 buttonMapAll.addEventListener("click", mapAllItems);
+buttonSumAll.addEventListener("click", sumAllItems);
+buttonFilterAll.addEventListener("click", filterAllItems);
