e4e98eb0442d7d13e804ec71a395514adb581f94
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-09 11:46:15 -0300

    foreach e reduce

--- 00-projetos/07-burguer/src/js/script.js
+++ 00-projetos/07-burguer/src/js/script.js
@@ -1,1 +1,33 @@
-console.log(menuOptions);
+const list = document.querySelector("ul");
+const buttonShowAll = document.querySelector(".show-all");
+const buttonMapAll = document.querySelector(".map-all");
+
+function showAll(productsArray) {
+  let myLi = ""; //resetar
+
+  productsArray.forEach((product) => {
+    myLi += `
+    <li>
+      <img src=${product.src} />
+      <p>${product.name}</p>
+      <p class="item-price">R$ ${product.price}</p>
+    </li>
+    
+    `;
+  });
+
+  list.innerHTML = myLi;
+}
+
+function mapAllItems() {
+  const newPrices = menuOptions.map((product) => ({
+    //spread operator - pega todos os itens do objeto
+    ...product,
+    price: product.price * 0.9, //dar 10% de desconto
+  }));
+
+  showAll(newPrices);
+}
+
+buttonShowAll.addEventListener("click", () => showAll(menuOptions));
+buttonMapAll.addEventListener("click", mapAllItems);
