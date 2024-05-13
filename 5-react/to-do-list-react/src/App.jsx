1ca80b371254714deb61a670474dcf99fc9c1345
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-13 17:06:53 -0300

    icones react

--- 5-react/to-do-list-react/src/App.jsx
+++ 5-react/to-do-list-react/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { v4 as uuid } from "uuid";
 
+import { FcEmptyTrash, FcOk } from "react-icons/fc";
+
 import { Container, ToDoList, Input, Button } from "./styles.js";
 
 function App() {
@@ -23,7 +25,11 @@ function App() {
           <Button onClick={clicarNoBotao}>Adicionar</Button>
           <ul>
             {list.map((item) => (
-              <li key={item.id}>{item.task}</li>
+              <div>
+                <FcOk />
+                <li key={item.id}>{item.task}</li>
+                <FcEmptyTrash />
+              </div>
             ))}
           </ul>
         </ToDoList>
