d603e036b9266e3eb10ff8c8014f4591fbe153be
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-13 16:55:00 -0300

    fontes no react

--- 5-react/to-do-list-react/src/globalStyles.js
+++ 5-react/to-do-list-react/src/globalStyles.js
@@ -6,7 +6,12 @@ const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    outline: none
+    outline: none;
+    
+  }
+
+  * {
+  font-family: "Roboto", sans-serif;
   }
 
 
