7adf87614f747cbf1688ba7f3ae7e372cd475fe1
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-15 14:35:37 -0300

    Estilizando background

--- 00-projetos/09-devmovies/src/pages/Home/index.jsx
+++ 00-projetos/09-devmovies/src/pages/Home/index.jsx
@@ -1,18 +1,20 @@
 import api from "../../services/api";
+import { Background } from "./styles";
 
 function Home() {
   async function getMovies() {
     const data = await api.get("/movie/popular");
+
     console.log(data);
   }
 
   getMovies();
 
   return (
-    <div>
+    <Background img="https://image.tmdb.org/t/p/original/kYgQzzjNis5jJalYtIHgrom0gOx.jpg">
       <h1>Home</h1>
       <p>Essa é a Home</p>
-    </div>
+    </Background>
   );
 }
 
