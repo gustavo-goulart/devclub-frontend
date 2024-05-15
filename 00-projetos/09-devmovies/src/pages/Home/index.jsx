a89257af02bec3ecbc59c67b205198bd3052f17c
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-15 15:34:41 -0300

    estilizando form e container

--- 00-projetos/09-devmovies/src/pages/Home/index.jsx
+++ 00-projetos/09-devmovies/src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 import api from "../../services/api";
-import { Background } from "./styles";
+import { Background, Container, Info, Poster } from "./styles";
 
 function Home() {
   const [movie, setMovie] = useState();
@@ -24,8 +24,18 @@ function Home() {
         <Background
           img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
         >
-          <h1>{movie.title}</h1>
-          <p>{movie.overview}</p>
+          <Container>
+            <Info>
+              <h1>{movie.title}</h1>
+              <p>{movie.overview}</p>
+            </Info>
+            <Poster>
+              <img
+                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                alt="capa-do-filme"
+              />
+            </Poster>
+          </Container>
         </Background>
       )}
     </>
--- 00-projetos/09-devmovies/src/pages/Home/styles.js
+++ 00-projetos/09-devmovies/src/pages/Home/styles.js
@@ -19,3 +19,40 @@ export const Background = styled.div`
     background-color: rgba(0, 0, 0, 0.5);
   }
 `;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 100vh;
+  max-width: 1500px;
+`;
+
+export const Info = styled.div`
+  z-index: 2;
+  padding: 30px;
+  width: 50%;
+
+  h1 {
+    font-size: 3.125rem;
+    font-weight: 700;
+    color: #fff;
+  }
+
+  p {
+    font-size: 1.2rem;
+    font-weight: 500;
+    color: #fff;
+    margin-top: 30px;
+    margin-bottom: 20px;
+  }
+`;
+
+export const Poster = styled.div`
+  z-index: 2;
+
+  img {
+    width: 280px;
+    border-radius: 30px;
+  }
+`;
