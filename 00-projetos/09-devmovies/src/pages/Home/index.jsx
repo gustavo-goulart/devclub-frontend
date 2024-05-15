81412e00301e0f1bd98a2071a22320d76074dbc4
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-15 15:12:44 -0300

    useEffect

--- 00-projetos/09-devmovies/src/pages/Home/index.jsx
+++ 00-projetos/09-devmovies/src/pages/Home/index.jsx
@@ -1,20 +1,34 @@
+import { useState, useEffect } from "react";
+
 import api from "../../services/api";
 import { Background } from "./styles";
 
 function Home() {
-  async function getMovies() {
-    const data = await api.get("/movie/popular");
+  const [movie, setMovie] = useState();
 
-    console.log(data);
-  }
+  useEffect(() => {
+    async function getMovies() {
+      const {
+        data: { results },
+      } = await api.get("/movie/popular");
 
-  getMovies();
+      setMovie(results[7]);
+    }
+
+    getMovies();
+  }, []);
 
   return (
-    <Background img="https://image.tmdb.org/t/p/original/kYgQzzjNis5jJalYtIHgrom0gOx.jpg">
-      <h1>Home</h1>
-      <p>Essa é a Home</p>
-    </Background>
+    <>
+      {movie && (
+        <Background
+          img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+        >
+          <h1>{movie.title}</h1>
+          <p>{movie.overview}</p>
+        </Background>
+      )}
+    </>
   );
 }
 
