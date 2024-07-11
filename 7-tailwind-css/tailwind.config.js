8314b42cc3f6e0a18061513062a4c83236236061
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-07-11 16:23:12 -0300

    finalizando js

---
+++ 7-tailwind-css/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "blue-facebook": "#1877f2",
+        "gray-facebook": "#f0f2f5",
+        "green-facebook": "#36a420",
+      },
+
+      spacing: {
+        580: "580px",
+        380: "380px",
+      },
+    },
+  },
+  plugins: [],
+};
